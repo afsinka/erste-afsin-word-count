b2c2ed82c5e1f2f1411de979523891289123ae34
Afsin Kapusuzoglu <afsin.kapusuzoglu@topia.com>
2020-12-21 15:53:01 +0200

    read from file feature is added

--- src/main/java/com/WordCount.java
+++ src/main/java/com/WordCount.java
@@ -16,19 +16,28 @@ import org.apache.commons.collections4.CollectionUtils;
 
 public class WordCount {
 
+  private static String BASE_FILE_PATH = "src/main/resources/";
   private static String STOP_WORDS_FILE_PATH = "src/main/resources/stopwords.txt";
 
   public static void main(String[] args) {
-    final List<String> excludedWords = readExcludedWords();
+    final List<String> excludedWords = readFile(STOP_WORDS_FILE_PATH);
+    String input = null;
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    System.out.print("Enter text:");
-    try {
-      String input = br.readLine();
-      System.out.println("Number of words:  " + countWordsWithRegex(input, excludedWords));
-    } catch (IOException e) {
-      e.printStackTrace();
+    if (args.length == 1) {
+      final String fileName = args[0];
+      List<String> wordsInFile = readFile(BASE_FILE_PATH + fileName);
+      input = String.join(" ", wordsInFile);
+    } else {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      System.out.print("Enter text:");
+      try {
+        input = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
+
+    System.out.println("Number of words: " + countWordsWithRegex(input, excludedWords));
   }
 
   /**
@@ -59,10 +68,10 @@ public class WordCount {
     return count;
   }
 
-  private static List<String> readExcludedWords() {
+  private static List<String> readFile(final String fileName) {
     List<String> list = new ArrayList<>();
 
-    try (Stream<String> stream = Files.lines(Paths.get(STOP_WORDS_FILE_PATH))) {
+    try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
       list = stream.collect(Collectors.toList());
     } catch (IOException e) {
       e.printStackTrace();
