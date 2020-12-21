77cef7c2cb588671bb3480fb2e39dca95d10a06f
Afsin Kapusuzoglu <afsin.kapusuzoglu@topia.com>
2020-12-21 15:32:24 +0200

    add excluded words feature

--- src/main/java/com/WordCount.java
+++ src/main/java/com/WordCount.java
@@ -3,16 +3,29 @@ package com;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.apache.commons.collections4.CollectionUtils;
 
 public class WordCount {
+
+  private static String STOP_WORDS_FILE_PATH = "src/main/resources/stopwords.txt";
+
   public static void main(String[] args) {
+    final List<String> excludedWords = readExcludedWords();
+
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     System.out.print("Enter text:");
     try {
       String input = br.readLine();
-      System.out.println("Number of words:  " + countWordsWithRegex(input));
+      System.out.println("Number of words:  " + countWordsWithRegex(input, excludedWords));
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -22,6 +35,13 @@ public class WordCount {
    * Returns count of words in a sentence
    */
   public static long countWordsWithRegex(final String text) {
+    return countWordsWithRegex(text, Collections.emptyList());
+  }
+
+  /**
+   * Returns count of words in a sentence except the ones in the exclusion list
+   */
+  public static long countWordsWithRegex(final String text, final List<String> excludedWords) {
     if (text == null || text.trim().isEmpty()) {
       return 0;
     }
@@ -29,8 +49,25 @@ public class WordCount {
     Matcher matcher = pattern.matcher(text);
     int count = 0;
     while (matcher.find()) {
-      count++;
+      String word = matcher.group();
+      if (CollectionUtils.isNotEmpty(excludedWords) && excludedWords.contains(word)) {
+        //skip
+      } else {
+        count++;
+      }
     }
     return count;
+  }
+
+  private static List<String> readExcludedWords() {
+    List<String> list = new ArrayList<>();
+
+    try (Stream<String> stream = Files.lines(Paths.get(STOP_WORDS_FILE_PATH))) {
+      list = stream.collect(Collectors.toList());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return list;
   }
 }
--- src/test/java/com/WordCountTest.java
+++ src/test/java/com/WordCountTest.java
@@ -2,6 +2,8 @@ package com;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 
 public class WordCountTest {
@@ -14,5 +16,22 @@ public class WordCountTest {
     assertEquals(5, WordCount.countWordsWithRegex("Marry had a little lamp"));
     assertEquals(2, WordCount.countWordsWithRegex("all4you"));
     assertEquals(3, WordCount.countWordsWithRegex("X.Y!Z"));
+  }
+
+  @Test
+  public void testCountWordsWithRegexAndExclusionList() {
+    List<String> excludedWords = new ArrayList<>();
+    excludedWords.add("the");
+    excludedWords.add("a");
+    excludedWords.add("on");
+    excludedWords.add("off");
+
+    assertEquals(0, WordCount.countWordsWithRegex("", excludedWords));
+    assertEquals(0, WordCount.countWordsWithRegex(" ", excludedWords));
+    assertEquals(0, WordCount.countWordsWithRegex(null, excludedWords));
+    assertEquals(4, WordCount.countWordsWithRegex("Marry had a little lamp", excludedWords));
+    assertEquals(2, WordCount.countWordsWithRegex("all4you", excludedWords));
+    assertEquals(3, WordCount.countWordsWithRegex("X.Y!Z", excludedWords));
+    assertEquals(0, WordCount.countWordsWithRegex("the a on off", excludedWords));
   }
 }
